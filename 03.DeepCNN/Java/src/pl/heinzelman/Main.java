cf6c4325afed169bfcf41cd9ec64208b9a66a70f
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2025-12-06 16:12:46 +0100

    !

---
+++ 03.DeepCNN/Java/src/pl/heinzelman/Main.java
@@ -0,0 +1,11 @@
+package pl.heinzelman;
+import pl.heinzelman.tasks.Task;
+import pl.heinzelman.tasks.Task_4_CNN;
+
+public class Main {
+    public static void main( String[] args ) {
+        Task task;
+        task = new Task_4_CNN();
+        task.doTask( 10 );
+    }
+}
---
+++ 03.DeepCNN/Java/src/pl/heinzelman/neu/Neuron.java
@@ -0,0 +1,41 @@
+package pl.heinzelman.neu;
+
+public class Neuron {
+    private float bias=0.0f;
+    private final float[] W;
+    private final LayerParent parent;
+    private final static float mu=0.001f;
+
+    public Neuron( int m, LayerParent parent ) {
+        this.parent=parent;
+        this.W = new float[m];
+    }
+
+
+    public void setWm( int m, float wji ){ W[m] = wji; }
+
+    public float Forward( float[] X ) {
+        float res=bias;
+        for ( int m=0; m<W.length; m++ ) {
+            res+= X[m]*W[m];
+        }
+        return res;
+    }
+
+    public void Backward( float en_x_dFIznI , float eIn ) {
+        float[] X = parent.getX();
+        for ( int m=0; m<W.length; m++ ) {
+            parent.getEout()[m] += ( W[m] * en_x_dFIznI );
+            W[m] = W[m] - ( mu * en_x_dFIznI * X[m] );
+        }
+    }
+
+    public void BackwardBias( float en_x_dFIznI ) {
+        bias = bias - ( mu * en_x_dFIznI );
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
